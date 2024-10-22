5bc497ca1beca1cf05966695c0af2bac85efb862
FerZva <fernandzva2019@outlook.es>
2024-10-22 10:47:21 -0600

    Removing no use import on app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Benefits from "./components/Benefits";
 import Disciplines from "./components/Disciplines";

7e425a6e5c3c51e0acd485e322d3289ffc8e9530
FerZva <fernandzva2019@outlook.es>
2024-10-27 09:34:10 -0600

    New pages created

---
+++ src/app/promartialarts/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const promartialarts = () => {
+  return <div>promartialarts</div>;
+};
+
+export default promartialarts;
---
+++ src/app/wbfhonduras/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const wbfhonduras = () => {
+  return <div>wbfhonduras</div>;
+};
+
+export default wbfhonduras;

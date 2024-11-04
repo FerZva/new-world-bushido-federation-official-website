86d445bbd77e6b15ab9405adaef158bbb9d24653
FerZva <fernandzva2019@outlook.es>
2024-11-03 22:26:37 -0600

    Mobile Header Menu created

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,28 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-// import { pages } from "../services/routes";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import Image from "next/image";
 import WBFHeaderLogo from "../assets/logo.webp";
 import { FaBagShopping } from "react-icons/fa6";
 import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 import { FiMenu } from "react-icons/fi";
-import MobileMenu from "./MobileMenu";
+import { IoClose } from "react-icons/io5";
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
+  const [collapse, setCollapse] = useState(true);
+
   const handleShowMenu = () => {
     setShowMenu(!showMenu);
   };
+
+  const handleCollapse = () => {
+    setCollapse(!collapse);
+  };
   return (
     <>
-      <header className="hidden w-full lg:flex justify-between px-1 py-2">
+      <header className="hidden w-full lg:flex justify-between px-1 py-2 relative">
         <Link href="/">
           <Image
             src={WBFHeaderLogo}
@@ -78,8 +84,71 @@ const Header = () => {
         </Link>
         <button>
           <FiMenu className="text-white text-[30px]" onClick={handleShowMenu} />
-          {showMenu && <MobileMenu />}
         </button>
+        {/* Mobile Menu */}
+        <div
+          className={`flex justify-end flex-col fixed top-0 ${
+            showMenu ? "left-[321px]" : "left-[0px]"
+          } w-full min-h-screen bg-slate-900/5 z-50 transition-all`}
+        >
+          <div className="w-90% min-h-screen bg-slate-900 px-1 py-2">
+            <div className="flex justify-between mb-6">
+              <Link href="/">
+                <Image
+                  src={WBFHeaderLogo}
+                  alt="World Bushido Federation header logo"
+                />
+              </Link>
+              <IoClose
+                className="text-white text-[30px]"
+                onClick={handleShowMenu}
+              />
+            </div>
+            <div className="flex flex-col px-2">
+              <p className="flex items-center mb-2" onClick={handleCollapse}>
+                World Bushido Federation&nbsp;
+                {collapse ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />}
+              </p>
+              <div
+                className={`flex flex-col ml-6 ${
+                  collapse ? "hidden" : "block"
+                } transition-all`}
+              >
+                <Link
+                  href="/wbfhonduras"
+                  className="mb-2"
+                  onClick={handleShowMenu}
+                >
+                  World Bushido Federation Honduras
+                </Link>
+                <Link
+                  href="/committee"
+                  className="mb-2"
+                  onClick={handleShowMenu}
+                >
+                  Committee
+                </Link>
+                <Link href="/about" className="mb-2" onClick={handleShowMenu}>
+                  About World Bushido Federation
+                </Link>
+              </div>
+              <Link
+                href="/membership"
+                className="mb-2"
+                onClick={handleShowMenu}
+              >
+                Memberships
+              </Link>
+              <Link
+                href="/promartialarts"
+                className="mb-2"
+                onClick={handleShowMenu}
+              >
+                Pro Martial Arts
+              </Link>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   );

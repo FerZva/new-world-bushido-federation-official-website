c43453956d8b7330bdcc63cb8db705ec9271b286
FerZva <fernandzva2019@outlook.es>
2025-05-22 20:40:14 -0600

    sucess page started

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,36 +1,7 @@
-"use client";
-import { useEffect } from "react";
-import axios from "axios";
-import { useSearchParams } from "next/navigation";
+import React from "react";
 
-export default function Success() {
-  const searchParams = useSearchParams();
-  const email = searchParams.get("email") || "cliente@ejemplo.com";
+const Success = () => {
+  return <div>Thank for your purchase</div>;
+};
 
-  useEffect(() => {
-    const sendEmail = async () => {
-      try {
-        await axios.post("/api/send-email", { email });
-        console.log("Correo enviado con éxito.");
-      } catch (error) {
-        console.error("Error al enviar el correo:", error);
-      }
-    };
-
-    sendEmail();
-  }, [email]);
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-green-50">
-      <div className="bg-white p-8 rounded-lg shadow-md text-center max-w-md">
-        <h1 className="text-2xl font-bold text-green-600 mb-4">
-          ¡Pago Exitoso! ✅
-        </h1>
-        <p className="text-gray-700">
-          Gracias por tu compra. Hemos enviado un correo de confirmación a{" "}
-          <strong>{email}</strong>.
-        </p>
-      </div>
-    </div>
-  );
-}
+export default Success;
